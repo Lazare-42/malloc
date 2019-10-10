c591b2bcc6a75bf106e591bf356807e2ca8fbda1
lazrossi <lazrossi@student.42.fr>
2019-10-10 11:14:23 +0200

    Added dummy realloc() function to three main functions file

--- srcs/three_main_functions.c
+++ srcs/three_main_functions.c
@@ -15,10 +15,12 @@ void free(void *ptr)
     /**
      *  Should first check if passed pointer belongs the all pointers.
      */
-    if (ptr_stc_global_static_manipulation_structure == NULL)
+    if (ptr_stc_global_static_manipulation_structure == NULL || ZERO == Fu8__bool_check_if_pointer_passed_to_free_was_previously_malloc(ptr_stc_global_static_manipulation_structure, ptr))
     {
+        ft_printf("[[red]]Rejecting free because pointer was not previously allocated[[end]]\n");
         return ;
     }
+    ft_printf("Entering free\n");
     ptr_stc_block_pointer_to_free                                           = (struct s_block*)(((uint8_t*)(ptr)) - sizeof(struct s_block));
     ptr_stc_block_pointer_to_free->u64_free_size_                           = ptr_stc_block_pointer_to_free->u64_size_;
     ptr_stc_page_storing_block_pointer                                      = ptr_stc_block_pointer_to_free->ptr_page_base_;
@@ -42,8 +44,6 @@ void    *malloc(size_t size)
     /**
      *  If the manipulation structure is set to NULL, initialize it.
      */
-    ft_printf("Coucou\n");
-        return (NULL);
     if (ptr_stc_global_static_manipulation_structure == NULL)
     {
         ptr_stc_global_static_manipulation_structure = Fptr_stc_manipulation__create_manipulation_structure();
@@ -61,18 +61,28 @@ void *realloc(void *ptr, size_t size)
     void            *ptr_void_lcl_realloced_memory;
 
     ptr_stc_lcl_block_pointer_to_realloc = NULL;
-    if (ptr_stc_global_static_manipulation_structure == NULL)
+    if (ptr_stc_global_static_manipulation_structure == NULL || ZERO == Fu8__bool_check_if_pointer_passed_to_free_was_previously_malloc(ptr_stc_global_static_manipulation_structure, ptr))
     {
-        return (NULL);
+        ft_printf("[[red]]Redirecting call to realloc to malloc() because pointer %p because pointer was not previously allocated[[end]]\n", ptr);
+        return (malloc(size));
     }
+    ft_printf("Entering realloc\n");
     ptr_stc_lcl_block_pointer_to_realloc = (struct s_block*)(((uint8_t*)(ptr)) - sizeof(struct s_block));
     if (ptr_stc_lcl_block_pointer_to_realloc->u64_size_ < size)
     {
         ptr_void_lcl_realloced_memory = malloc(size);
-        ft_memcpy(ptr_void_lcl_realloced_memory, (void*)(((uint8_t*)ptr_stc_lcl_block_pointer_to_realloc) + Fu64__align16(sizeof(struct s_block))), ptr_stc_lcl_block_pointer_to_realloc->u64_size_);
+        memcpy(ptr_void_lcl_realloced_memory, (void*)(((uint8_t*)ptr_stc_lcl_block_pointer_to_realloc) + Fu64__align16(sizeof(struct s_block))), ptr_stc_lcl_block_pointer_to_realloc->u64_size_);
         free((void*)(((uint8_t*)ptr_stc_lcl_block_pointer_to_realloc) + Fu64__align16(sizeof(struct s_block))));
         return (ptr_void_lcl_realloced_memory);
     }
     ptr_stc_lcl_block_pointer_to_realloc->u64_free_size_ = ptr_stc_lcl_block_pointer_to_realloc->u64_size_ - size;
     return ((void*)(((uint8_t*)ptr_stc_lcl_block_pointer_to_realloc) + Fu64__align16(sizeof(struct s_block))));
+}
+
+void *calloc(size_t elemn, size_t size)
+{
+    ft_printf("[[red]]In caloc, function not created yet ![[end]]\n");
+    (void)elemn;
+    (void)size;
+    return NULL;
 }
