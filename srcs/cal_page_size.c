4f2c51b35f13997febd316c8f3693c2c9bfe53f4
lazrossi <lazrossi@student.42.fr>
2019-10-13 15:40:27 +0200

    Advancing. My eyes are bleeding

--- srcs/cal_page_size.c
+++ srcs/cal_page_size.c
@@ -3,76 +3,84 @@
 #include <sys/mman.h>
 #include <string.h>
 
-    //ptr_pssd_manipulation_structure->ptr_stc_container_to_alloc_realloc->ptr_stc_block_container = (struct s_block  **)((uint8_t*)&(ptr_pssd_manipulation_structure->ptr_stc_container_to_alloc_realloc[sizeof(struct s_block_container) - sizeof(struct s_block  **)]));
-
-uint64_t fu64__align16(size_t number)
+uint64_t	fu64__align16(size_t number)
 {
-    return ((uint64_t)(((number - 1) | (16 - 1)) + 1));
+	return ((uint64_t)(((number - 1) | (16 - 1)) + 1));
 }
 
-uint8_t  fu8_bool__check_if_chosen_page_size_too_big(uint64_t u64_next_page_size, uint64_t u64_pssd_required_size)
+uint8_t		fu8_bool__check_if_chosen_page_size_too_big(
+		uint64_t u64_next_page_size, uint64_t u64_pssd_required_size)
 {
-    if (u64_next_page_size > (u64_pssd_required_size << 1) && u64_pssd_required_size > TINY)
-    {
-        return ((uint8_t)1);
-    }
-    return ((uint8_t)ZERO);
+	if (u64_next_page_size > (u64_pssd_required_size << 1)
+			&& u64_pssd_required_size > TINY)
+	{
+		return ((uint8_t)1);
+	}
+	return ((uint8_t)ZERO);
 }
 
-uint64_t fu64__return_lower_page_size(uint64_t u64_pssd_next_page_size, uint64_t u64_pssd_required_size)
+uint64_t	fu64__return_lower_page_size(
+		uint64_t u64_pssd_next_page_size, uint64_t u64_pssd_required_size)
 {
-    uint64_t u64_lcl_new_page_size;
+	uint64_t u64_lcl_new_page_size;
 
-    u64_lcl_new_page_size = ZERO;
-    u64_lcl_new_page_size = u64_pssd_next_page_size;
-    while (u64_lcl_new_page_size > (u64_pssd_required_size << 1))
-    {
-    u64_lcl_new_page_size = u64_lcl_new_page_size >> 1;
-    }
-    return (u64_lcl_new_page_size);
+	u64_lcl_new_page_size = ZERO;
+	u64_lcl_new_page_size = u64_pssd_next_page_size;
+	while (u64_lcl_new_page_size > (u64_pssd_required_size << 1))
+	{
+		u64_lcl_new_page_size = u64_lcl_new_page_size >> 1;
+	}
+	return (u64_lcl_new_page_size);
 }
 
-uint64_t fu64__return_upper_page_size(uint64_t u64_previous_page_size, uint64_t u64_pssd_required_size)
+uint64_t	fu64__return_upper_page_size(uint64_t u64_previous_page_size,
+		uint64_t u64_pssd_required_size)
 {
-    uint64_t u64_lcl_new_page_size;
+	uint64_t u64_lcl_new_page_size;
 
-    u64_lcl_new_page_size = ZERO;
-    u64_lcl_new_page_size = (u64_previous_page_size << 1);
-    while (u64_lcl_new_page_size < u64_pssd_required_size) 
-    {
-    u64_lcl_new_page_size = (u64_lcl_new_page_size << 1);
-    }
-    return (u64_lcl_new_page_size);
+	u64_lcl_new_page_size = ZERO;
+	u64_lcl_new_page_size = (u64_previous_page_size << 1);
+	while (u64_lcl_new_page_size < u64_pssd_required_size)
+	{
+		u64_lcl_new_page_size = (u64_lcl_new_page_size << 1);
+	}
+	return (u64_lcl_new_page_size);
 }
 
-uint64_t fu64_get_upper_page_size_for_requested_size(struct s_manipulation *ptr_pssd_manipulation_structure, uint64_t u64_pssd_requested_size)
+uint64_t	fu64_get_upper_page_size_for_requested_size(struct s_manipulation
+		*ptr_pssd_manipulation_structure, uint64_t u64_pssd_requested_size)
 {
-    uint64_t    u64_lcl_upper_page_size;
+	uint64_t u64_lcl_upper_page_size;
 
-    u64_lcl_upper_page_size = ZERO;
-    u64_lcl_upper_page_size = (float)(((u64_pssd_requested_size 
-    / ptr_pssd_manipulation_structure->u64_pagesize) + 1)
-    - (float)((float)(u64_pssd_requested_size)
-    / (float)ptr_pssd_manipulation_structure->u64_pagesize))
-    * (float)ptr_pssd_manipulation_structure->u64_pagesize;
-    return (u64_lcl_upper_page_size + u64_pssd_requested_size);
+	u64_lcl_upper_page_size = ZERO;
+	u64_lcl_upper_page_size = (float)(((u64_pssd_requested_size
+	/ ptr_pssd_manipulation_structure->u64_pagesize) + 1)
+	- (float)((float)(u64_pssd_requested_size)
+	/ (float)ptr_pssd_manipulation_structure->u64_pagesize))
+	* (float)ptr_pssd_manipulation_structure->u64_pagesize;
+	return (u64_lcl_upper_page_size + u64_pssd_requested_size);
 }
 
-uint64_t fu64__get_default_page_size_for_page_block(struct s_manipulation *ptr_pssd_manipulation_structure, uint64_t u64_pssd_block_size, uint64_t u64_pssd_number_of_elements)
+uint64_t	fu64__get_default_page_size_for_page_block(
+		struct s_manipulation *ptr_pssd_manipulation_structure,
+		uint64_t u64_pssd_block_size, uint64_t u64_pssd_number_of_elements)
 {
-    uint64_t    u64_lcl_requested_size;
+	uint64_t	u64_lcl_requested_size;
 
-    u64_lcl_requested_size  = ZERO;
-
-    u64_lcl_requested_size = fu64__align16(sizeof(struct s_page))
-    + (u64_pssd_number_of_elements != ZERO ? u64_pssd_number_of_elements : 1)
-    * (fu64__align16(sizeof(struct s_block)) +  fu64__align16(u64_pssd_block_size));
-
-    return (fu64_get_upper_page_size_for_requested_size(ptr_pssd_manipulation_structure, u64_lcl_requested_size));
+	u64_lcl_requested_size = ZERO;
+	u64_lcl_requested_size = fu64__align16(sizeof(struct s_page))
+	+ (u64_pssd_number_of_elements != ZERO ? u64_pssd_number_of_elements : 1)
+	* (fu64__align16(sizeof(struct s_block))
+	+ fu64__align16(u64_pssd_block_size));
+	return (fu64_get_upper_page_size_for_requested_size(
+			ptr_pssd_manipulation_structure, u64_lcl_requested_size));
 }
 
-uint64_t fu64__get_number_of_structure_in_asked_page(uint64_t u64_pssd_asked_page_size, uint64_t u64_pssd_asked_malloc_size)
+uint64_t	fu64__get_number_of_structure_in_asked_page(
+		uint64_t u64_pssd_asked_page_size, uint64_t u64_pssd_asked_malloc_size)
 {
-    //ft_printf("[[blue]]Default number of structures is %20llu[[end]]\n", (u64_pssd_asked_page_size - fu64__align16(sizeof(struct s_page))) / (fu64__align16(sizeof(struct s_block)) +  fu64__align16(u64_pssd_asked_malloc_size)));
-    return ((u64_pssd_asked_page_size - fu64__align16(sizeof(struct s_page))) / (fu64__align16(sizeof(struct s_block)) +  fu64__align16(u64_pssd_asked_malloc_size)));
+	return ((u64_pssd_asked_page_size
+	- fu64__align16(sizeof(struct s_page)))
+	/ (fu64__align16(sizeof(struct s_block))
+	+ fu64__align16(u64_pssd_asked_malloc_size)));
 }
--- srcs/malloc.c
+++ srcs/malloc.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*																			  */
-/*														  :::	   ::::::::   */
-/*	 malloc.c											:+:		 :+:	:+:   */
-/*													  +:+ +:+		  +:+	  */
-/*	 By: lazrossi <marvin@42.fr>					+#+  +:+	   +#+		  */
-/*												  +#+#+#+#+#+	+#+			  */
-/*	 Created: 2019/10/07 18:01:52 by lazrossi		   #+#	  #+#			  */
-/*	 Updated: 2019/10/13 11:42:51 by lazrossi		  ###	########.fr		  */
-/*																			  */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lazrossi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/13 15:23:42 by lazrossi          #+#    #+#             */
+/*   Updated: 2019/10/13 15:33:03 by lazrossi         ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
@@ -15,104 +15,130 @@
 #include <sys/mman.h>
 #include "libft.h"
 
-struct s_page *fu8__add_upper_page_category_or_extend_current_page_category(struct s_manipulation *ptr_pssd_stc_manipulation_structure, struct s_page *ptr_pssd_stc_last_page, uint64_t u64_pssd_required_size)
+static struct s_page	*add_or_extend(
+		struct s_manipulation *ptr_pssd_stc_manipulation_structure,
+		struct s_page *ptr_pssd_stc_last_page, uint64_t u64_pssd_required_size)
 {
 	if (ptr_pssd_stc_last_page->u64_block_size_ < u64_pssd_required_size)
 	{
-		if (NULL != (ptr_pssd_stc_last_page->ptr_next_page_upper_category_ = fu8__create_and_init_new_page_category(ptr_pssd_stc_manipulation_structure, fu64__return_upper_page_size(ptr_pssd_stc_last_page->u64_block_size_, u64_pssd_required_size), ZERO)))
-		{
+		if (NULL != (ptr_pssd_stc_last_page->ptr_next_page_upper_category_ =
+		fu8__create_and_init_new_page_category(
+			ptr_pssd_stc_manipulation_structure,
+		fu64__return_upper_page_size(ptr_pssd_stc_last_page->u64_block_size_,
+				u64_pssd_required_size), ZERO)))
 			return (ptr_pssd_stc_last_page->ptr_next_page_upper_category_);
-		}
 	}
-	if (NULL != (ptr_pssd_stc_last_page->ptr_next_page_in_category_ = fu8__create_and_init_new_page_category(ptr_pssd_stc_manipulation_structure, ptr_pssd_stc_last_page->u64_block_size_, ZERO)))
-	{
+	if (NULL != (ptr_pssd_stc_last_page->ptr_next_page_in_category_ =
+	fu8__create_and_init_new_page_category(ptr_pssd_stc_manipulation_structure,
+		ptr_pssd_stc_last_page->u64_block_size_, ZERO)))
 		return (ptr_pssd_stc_last_page->ptr_next_page_in_category_);
-	}
 	return (NULL);
 }
 
-struct s_page *fptr_page__return_corresponding_page_category(struct s_manipulation *ptr_pssd_stc_manipulation_structure, uint64_t u64_pssd_required_size)
+static struct s_page	*fptr_page__return_corresponding_page_category(
+	struct s_manipulation *ptr_pssd_stc_manipulation_structure,
+	uint64_t u64_pssd_required_size)
 {
-	struct s_page					*ptr_stc_lcl_browse_page_category;
-	struct s_page					*ptr_stc_lcl_previously_browsed_page;
+	struct s_page					*browse;
+	struct s_page					*previously_browsed;
 
-	ptr_stc_lcl_browse_page_category		= NULL;
-	ptr_stc_lcl_previously_browsed_page		= NULL;
-	ptr_stc_lcl_browse_page_category	   = ptr_pssd_stc_manipulation_structure->ptr_stc_page_linked_list;
-	while (ptr_stc_lcl_browse_page_category != NULL && u64_pssd_required_size > ptr_stc_lcl_browse_page_category->u64_block_size_)
+	browse = ptr_pssd_stc_manipulation_structure->ptr_stc_page_linked_list;
+	while (browse != NULL && u64_pssd_required_size > browse->u64_block_size_)
 	{
-	ptr_stc_lcl_previously_browsed_page = ptr_stc_lcl_browse_page_category;
-	ptr_stc_lcl_browse_page_category = ptr_stc_lcl_browse_page_category->ptr_next_page_upper_category_;
+		previously_browsed = browse;
+		browse = browse->ptr_next_page_upper_category_;
 	}
-	if (ptr_stc_lcl_browse_page_category != NULL && fu8_bool__check_if_chosen_page_size_too_big(ptr_stc_lcl_browse_page_category->u64_block_size_, u64_pssd_required_size))
+	if (browse != NULL && fu8_bool__check_if_chosen_page_size_too_big(
+		browse->u64_block_size_, u64_pssd_required_size))
 	{
-		fu8__add_upper_page_category_or_extend_current_page_category(ptr_pssd_stc_manipulation_structure, ptr_stc_lcl_previously_browsed_page, u64_pssd_required_size);
-		if (NULL == ptr_stc_lcl_previously_browsed_page->ptr_next_page_upper_category_)
+		add_or_extend(ptr_pssd_stc_manipulation_structure,
+		previously_browsed, u64_pssd_required_size);
+		if (NULL == previously_browsed->ptr_next_page_upper_category_)
 			return (NULL);
-		ptr_stc_lcl_previously_browsed_page->ptr_next_page_upper_category_->ptr_next_page_upper_category_ = ptr_stc_lcl_browse_page_category;
-		ptr_stc_lcl_browse_page_category = ptr_stc_lcl_previously_browsed_page->ptr_next_page_upper_category_;
+		previously_browsed->ptr_next_page_upper_category_->
+		ptr_next_page_upper_category_ = browse;
+		browse = previously_browsed->ptr_next_page_upper_category_;
 	}
-	if (ptr_stc_lcl_browse_page_category == NULL) 
-	{
-		fu8__add_upper_page_category_or_extend_current_page_category(ptr_pssd_stc_manipulation_structure, ptr_stc_lcl_previously_browsed_page, u64_pssd_required_size);
-		if (NULL == (ptr_stc_lcl_browse_page_category = ptr_stc_lcl_previously_browsed_page->ptr_next_page_upper_category_))
-			return (NULL);
-	}
-	return (ptr_stc_lcl_browse_page_category);
+	if (browse == NULL && add_or_extend(ptr_pssd_stc_manipulation_structure,
+		previously_browsed, u64_pssd_required_size) && (NULL == (browse =
+		previously_browsed->ptr_next_page_upper_category_)))
+		return (NULL);
+	return (browse);
 }
 
-struct s_page *fptr_page__return_first_free_block_from_page_category(struct s_manipulation *ptr_pssd_stc_manipulation_structure, struct s_page *ptr_pssd_stc_browse_page_category)
+static struct s_page	*fptr_page__return_first_free_block_from_page_category(
+		struct s_manipulation *ptr_pssd_stc_manipulation_structure,
+		struct s_page *browse_page_category)
 {
-	struct s_page					*ptr_stc_lcl_base_page_category;
+	struct s_page					*base_page_category;
 	struct s_page					*ptr_stc_lcl_previously_browsed_page;
 
-	ptr_stc_lcl_previously_browsed_page		= NULL;
-	ptr_stc_lcl_base_page_category			= ptr_pssd_stc_browse_page_category;
-	while (ptr_pssd_stc_browse_page_category != NULL && ptr_pssd_stc_browse_page_category->ptr_first_free_block_ == NULL)
+	ptr_stc_lcl_previously_browsed_page = NULL;
+	base_page_category = browse_page_category;
+	while (browse_page_category != NULL
+		&& browse_page_category->ptr_first_free_block_ == NULL)
 	{
-	ptr_stc_lcl_previously_browsed_page = ptr_pssd_stc_browse_page_category;
-	ptr_pssd_stc_browse_page_category = ptr_pssd_stc_browse_page_category->ptr_next_page_in_category_;
+		ptr_stc_lcl_previously_browsed_page = browse_page_category;
+		browse_page_category = browse_page_category->ptr_next_page_in_category_;
 	}
-	if (ptr_pssd_stc_browse_page_category == NULL) 
+	if (browse_page_category == NULL)
 	{
-		fu8__add_upper_page_category_or_extend_current_page_category(ptr_pssd_stc_manipulation_structure, ptr_stc_lcl_previously_browsed_page, ptr_stc_lcl_previously_browsed_page->u64_block_size_);
-		if (NULL == (ptr_pssd_stc_browse_page_category = ptr_stc_lcl_previously_browsed_page->ptr_next_page_in_category_))
-		{
+		add_or_extend(
+		ptr_pssd_stc_manipulation_structure, ptr_stc_lcl_previously_browsed_page
+		, ptr_stc_lcl_previously_browsed_page->u64_block_size_);
+		if (NULL == (browse_page_category =
+			ptr_stc_lcl_previously_browsed_page->ptr_next_page_in_category_))
 			return (NULL);
-		}
-		ptr_stc_lcl_base_page_category->u64_total_number_of_pages_in_category_	+= 1;
-		ptr_stc_lcl_base_page_category->u64_number_of_used_pages_in_category_	+= 1;
+		base_page_category->u64_total_number_of_pages_in_category_ += 1;
+		base_page_category->u64_number_of_used_pages_in_category_ += 1;
 	}
-	ptr_pssd_stc_browse_page_category->ptr_base_page_category_				= ptr_stc_lcl_base_page_category;
+	browse_page_category->ptr_base_page_category_ =
+		base_page_category;
+	return (browse_page_category);
+}
+
+static struct s_page	*fptr_page__return_page_from_category(struct
+		s_manipulation *ptr_pssd_stc_manipulation_structure,
+		uint64_t u64_pssd_required_size,
+		struct s_page *ptr_pssd_stc_browse_page_category)
+{
+	if (NULL == (ptr_pssd_stc_browse_page_category =
+	fptr_page__return_corresponding_page_category(
+	ptr_pssd_stc_manipulation_structure, u64_pssd_required_size)))
+		return (NULL);
+	if (NULL == (ptr_pssd_stc_browse_page_category =
+				fptr_page__return_first_free_block_from_page_category(
+			ptr_pssd_stc_manipulation_structure,
+		ptr_pssd_stc_browse_page_category)))
+		return (NULL);
 	return (ptr_pssd_stc_browse_page_category);
 }
 
-void	*fptr_void__return_memory(struct s_manipulation *ptr_pssd_stc_manipulation_structure, uint64_t u64_pssd_required_size)
+void					*fptr_void__return_memory(struct s_manipulation
+		*ptr_pssd_stc_manipulation_structure, uint64_t u64_pssd_required_size)
 {
 	struct s_page					*ptr_stc_lcl_browse_page_category;
 	struct s_page					*ptr_stc_lcl_previously_browsed_page;
 	struct s_block					*ptr_stc_lcl_memory_block_to_return;
 
-	ptr_stc_lcl_browse_page_category		= NULL;
-	ptr_stc_lcl_previously_browsed_page		= NULL;
-	ptr_stc_lcl_memory_block_to_return		= NULL;
-	if (NULL == (ptr_stc_lcl_browse_page_category = fptr_page__return_corresponding_page_category(ptr_pssd_stc_manipulation_structure, u64_pssd_required_size)))
-	{
+	ptr_stc_lcl_browse_page_category = NULL;
+	ptr_stc_lcl_previously_browsed_page = NULL;
+	ptr_stc_lcl_memory_block_to_return = NULL;
+	if (NULL == (ptr_stc_lcl_browse_page_category =
+	fptr_page__return_page_from_category(
+	ptr_pssd_stc_manipulation_structure,
+	u64_pssd_required_size, ptr_stc_lcl_browse_page_category)))
 		return (NULL);
-	}
-	if (NULL == (ptr_stc_lcl_browse_page_category = fptr_page__return_first_free_block_from_page_category(ptr_pssd_stc_manipulation_structure, ptr_stc_lcl_browse_page_category)))
-	{
-		return (NULL);
-	}
-	ptr_stc_lcl_memory_block_to_return = ptr_stc_lcl_browse_page_category->ptr_first_free_block_;
+	ptr_stc_lcl_memory_block_to_return =
+		ptr_stc_lcl_browse_page_category->ptr_first_free_block_;
 	ptr_stc_lcl_browse_page_category->ptr_first_free_block_ =
-        ptr_stc_lcl_memory_block_to_return->ptr_next_block_in_page_;
+		ptr_stc_lcl_memory_block_to_return->ptr_next_block_in_page_;
 	ptr_stc_lcl_memory_block_to_return->ptr_next_block_in_page_ =
-        ptr_stc_lcl_browse_page_category->ptr_first_occuppied_block_;
+		ptr_stc_lcl_browse_page_category->ptr_first_occuppied_block_;
 	ptr_stc_lcl_browse_page_category->ptr_first_occuppied_block_ =
-        ptr_stc_lcl_memory_block_to_return;
+		ptr_stc_lcl_memory_block_to_return;
 	ptr_stc_lcl_memory_block_to_return->u64_free_size_ -=
-        u64_pssd_required_size;
-	return ((void*)(((uint8_t*)ptr_stc_lcl_memory_block_to_return) + fu64__align16(sizeof(struct s_block))));
+		u64_pssd_required_size;
+	return ((void*)(((uint8_t*)ptr_stc_lcl_memory_block_to_return)
+				+ fu64__align16(sizeof(struct s_block))));
 }
-
